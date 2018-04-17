70ca87047e900cb0e381bddc438c8d4c8e8031ca
ShaunaB93 <35816763+ShaunaB93@users.noreply.github.com>
2018-04-17 22:25:05 +0100

    Create Histograms.py
    firstcol etc uses the same code to divide the columns. Each of the plots to follow show the details of each of the specified columns of data from the dataset represented on a histogram. "xlabel" pieces of code were used to show labels so that the data represented on each histogram could be identified by any user looking. The outputs have been compared to many similar analysis completed which appear online and give similar results, such as those printed at https://machinelearningmastery.com/machine-learning-in-python-step-by-step/.

---
+++ Histograms.py
@@ -0,0 +1,30 @@
+#Shauna Byrne 11/04/18
+#Project Test Code from 2nd video 09/04/2018
+#https://matplotlib.org/users/pyplot_tutorial.html - used to label the xaxis with the relevant descriptions
+#Running code as Histograms.py in ipython leads to the histograms appearing one after the other once the previous diagram has been closed
+
+import numpy
+
+data = numpy.genfromtxt('data/iris1.csv', delimiter=',')
+
+firstcol = data[:,0]
+secondcol = data[:,1]
+thirdcol = data[:,2]
+forthcol = data[:3]
+
+import matplotlib.pyplot as plot
+plot.hist(firstcol)
+plot.xlabel('Sepal Length')
+plot.show()
+
+plot.hist(secondcol)
+plot.xlabel('Sepal Width')
+plot.show()
+
+plot.hist(thirdcol)
+plot.xlabel('Petal Length')
+plot.show()
+
+plot.hist(secondcol)
+plot.xlabel('Petal Width')
+plot.show()
