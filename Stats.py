4dbbf5aca858c42ffeb452c645943df7c5c47dea
ShaunaB93 <35816763+ShaunaB93@users.noreply.github.com>
2018-04-18 22:16:31 +0100

    Create Stats.py
    Additional code to calculate standard deviation, median, mode & variance for each of the columns in the data set to be compiled into the main body of the code in a later commit.

---
+++ Stats.py
@@ -0,0 +1,53 @@
+#Shauna Byrne 15/04/18
+#To be added to main calculations of mean,min & max
+
+import numpy as numpy
+from scipy import stats
+
+data = numpy.genfromtxt('data/iris1.csv', delimiter=',')
+#numpy.set_printoptions(precision = 3)
+
+firstcol = data[:,0]
+secondcol = data[:,1]
+thirdcol = data[:,2]
+fourthcol = data[:3]
+
+stdevfirstcol = numpy.std(firstcol)
+stdevsecondcol = numpy.std(secondcol)
+stdevthirdcol = numpy.std(thirdcol)
+stdevfourthcol = numpy.std(fourthcol)
+
+print("The standard deviation of the first column is: ", stdevfirstcol)
+print("The standard deviation of the second column is: ", stdevsecondcol)
+print("The standard deviation of the third column is: ", stdevthirdcol)
+print("The standard deviation of the fourth column is: ", stdevfourthcol)
+
+medianfirstcol = numpy.median(firstcol)
+mediansecondcol = numpy.median(secondcol)
+medianthirdcol = numpy.median(thirdcol)
+medianfourthcol = numpy.median(fourthcol)
+
+print("The median of the first column is: ", medianfirstcol)
+print("The median of the second column is: ", mediansecondcol)
+print("The median of the third column is: ", medianthirdcol)
+print("The median of the fourth column is: ", medianfourthcol)
+
+modefirstcol = stats.mode(firstcol)
+modesecondcol = stats.mode(secondcol)
+modethirdcol = stats.mode(thirdcol)
+modefourthcol = stats.mode(fourthcol)
+
+print("The mode of the first column is: ", modefirstcol)
+print("The mode of the second column is: ", modesecondcol)
+print("The mode of the third column is: ", modethirdcol)
+print("The mode of the fourth column is: ", modefourthcol)
+
+varifirstcol = numpy.var(firstcol)
+varisecondcol = numpy.var(secondcol)
+varithirdcol = numpy.var(thirdcol)
+varifourthcol = numpy.var(fourthcol)
+
+print("The variance of the first column is: ", varifirstcol)
+print("The variance of the second column is: ", varisecondcol)
+print("The variance of the third column is: ", varithirdcol)
+print("The variance of the fourth column is: ", varifourthcol)
